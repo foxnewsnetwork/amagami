84fba4d69574f7357986fbe0714ab23ee98dcdd3
Thomas Chen <tom.chen@am.sony.com>
2017-01-08 15:42:19 -0800

    lg version of bullet goal details scaffolded

--- addon/components/amagami-bullet-image-slide.js
+++ addon/components/amagami-bullet-image-slide.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import layout from '../templates/components/amagami-bullet-image-slide';
-
-export default Ember.Component.extend({
-  layout
+import ContainerMixin from 'flexi/mixins/container';
+export default Ember.Component.extend(ContainerMixin, {
+  layout,
+  classNames: ['amagami-bullet-image-slide']
 });
